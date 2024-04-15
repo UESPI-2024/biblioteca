19d99a98425091db57e69e2dd55735bebb754a9a
fabbioSborges <fabbioanderson@gmail.com>
2024-04-15 09:52:41 -0300

    fix: passa a informação se é do tipo admin

--- src/modules/user/create_user/create_user_controller.ts
+++ src/modules/user/create_user/create_user_controller.ts
@@ -3,10 +3,10 @@ import { Create_user_use_case } from "./create_user_use_case";
 
 export class Create_user_controller{
   async handle(req: Request, res: Response) {
-    const {email, password, name,idade, biblioteca_id } = req.body
+    const {email, password, name,idade, biblioteca_id,admin } = req.body
     try{ 
       const create_user_use_case = new Create_user_use_case()
-      const result = await create_user_use_case.execute({email, password, name, idade, biblioteca_id})
+      const result = await create_user_use_case.execute({email, password, name, idade, biblioteca_id, admin})
      return res.json({"Messagem": result})  
     }catch (e){
       return res.status(400).json({message: `Não foi possive criar usuario - ${e}`})
